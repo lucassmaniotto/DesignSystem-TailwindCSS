9a9ee32082528a5c71e2caa8e00e531e7169487a
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2024-06-24 12:13:15 -0300

    feat: componente de Switch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import { Notice } from "@/components/Notice/Notice";
 
 export default function Home() {
   return (
     <main>
-      <Notice type="success" message="Este é um aviso de sucesso!" />
     </main>
   );
 }
